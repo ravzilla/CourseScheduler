669d3c746eb85ca1900797302629fb824713393a
Ravi Ponmalai <rponmalai@gmail.com>
2015-11-04 20:46:37 +0000

    time parser changes it to 24hr time now so thats fun. i need to figure out what to do next. probably the scheduling algorithm

--- scheduler/src/main/reader.py
+++ scheduler/src/main/reader.py
@@ -20,6 +20,7 @@ def return_class(school:str,code:str,name:str)->list:
            if l[0] == school and l[1]==code:
                return parse_class(lineNum,wF)
                break
+          
 def parse_class(lineNum:int,data:list)->list: 
     
     classes =[]
@@ -30,11 +31,14 @@ def parse_class(lineNum:int,data:list)->list:
         if len(l)!=0:
                
             if l[0] != 'CCode' and l[0] != '~':
-              classes.append([int(l[0]),l[1],(l[5])])
+                if l[6] != '*TBA*' :
+                  print(parse_time([l[6],l[7]]))
+                classes.append([int(l[0]),l[1],(l[5])])
               
         else:
             break
     return classes
+    
 def parse_day(s:str)->list:
     result = [False]*5
     t= ['M','Tu','W','Th','F']
@@ -42,3 +46,28 @@ def parse_day(s:str)->list:
         if t[i] in s:
             result[i] = True
     return result
+    
+def parse_time(comb:list):
+    s=''
+    if comb[0][-1] =='-':
+        s=comb[0]+comb[1]
+    else:
+        s=comb[0]
+    
+    pm = False
+    if s[-1] == 'p' and s[0:2] !='12':
+        pm = True
+        s=s[:-1]
+    elif s[0:2] == '12':
+        s=s[:-1]
+    
+    time = []
+    for i,v in enumerate(s.split('-')[0].split(':')+s.split('-')[1].split(':')):
+        time.append(int(v))
+    if pm:
+        time[0] += 12
+        time[2] += 12
+        
+    pm = False
+  
+    return time
