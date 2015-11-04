638794dc93e3465da41f149d3822f62c0ead92ec
Ravi Ponmalai <rponmalai@gmail.com>
2015-11-04 07:45:44 +0000

    reads and returns list of classes with some arbitrary feats. next most important task is parsing times correctlly.

--- scheduler/src/main/reader.py
+++ scheduler/src/main/reader.py
@@ -11,27 +11,34 @@ def read_file(name:str)->list:
     f.close()
     return data
     
-def return_class(school:str,code:str,name:str)->int:
+def return_class(school:str,code:str,name:str)->list:
     wF = read_file(name)
     for lineNum,data in enumerate(wF):
         l= data.split()
         
         if len(l)!=0:
            if l[0] == school and l[1]==code:
-               print(parse_class(lineNum,wF))
+               return parse_class(lineNum,wF)
                break
 def parse_class(lineNum:int,data:list)->list: 
-    """times = []
-    days = []
-    code = 0"""
-    classTypes = []    
-    for n,d in enumerate(data[lineNum:]):
+    
+    classes =[]
+    for n,d in enumerate(data[lineNum+1:]):
         
         l = d.split()
        
         if len(l)!=0:
-            if 'cCode' != l[0]!='~' :
-                print(l)
-                if  l[1] not in classTypes:
-                    classTypes.append(l[1])
-    return classTypes
+               
+            if l[0] != 'CCode' and l[0] != '~':
+              classes.append([int(l[0]),l[1],(l[5])])
+              
+        else:
+            break
+    return classes
+def parse_day(s:str)->list:
+    result = [False]*5
+    t= ['M','Tu','W','Th','F']
+    for i in range(0,len(t)):
+        if t[i] in s:
+            result[i] = True
+    return result
